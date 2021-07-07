6cfa68b6d0e3109347c8b2fb0041d66ce7a91bb2
keisuke-5837 <86215950+keisuke-5837@users.noreply.github.com>
2021-07-07 16:51:44 +0900

    4-17の実装完了

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,6 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:edit, :show]
+
   def index
     @prototypes = Prototype.all
   end
@@ -18,18 +20,28 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    @prototype = Prototype.find(params[:id])
+    @prototype.update(prototype_params)
+    if @prototype.save
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit
+    end
   end
 
   private
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
   end
 end
